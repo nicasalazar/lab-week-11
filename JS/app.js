993815d00c455163fc511169e87b8da0b23ee804
nicasalazar <60679947+nicasalazar@users.noreply.github.com>
2021-03-25 23:11:11 -0700

    create toDoList array

--- JS/app.js
+++ JS/app.js
@@ -1,1 +1,19 @@
-// your JavaScript goes here, you can delete this comment
+const todoList = [
+    {
+        todo: 'clone repo for starter code',
+        status: 'complete'
+    },
+    {
+        todo: 'change the remote URL',
+        status: 'complete'
+    },
+    {
+        todo: 'follow part 2-6 instructions',
+        status: 'started'
+    },
+    {
+        todo: 'push your code to your own GitHub repo',
+        status: 'started'
+    }
+];
+
